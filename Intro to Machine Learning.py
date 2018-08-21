4534c54c16af7d3bf2a350620bd4532ea2c1bef0
devedean <devedean@yahoo.com>
2018-08-20 20:38:29 -0700

    Add files via upload

--- Intro to Machine Learning.py
+++ Intro to Machine Learning.py
@@ -1,6 +1,89 @@
 import mglearn
 import matplotlib.pyplot as plt
 
+def ThreeSources():
+ import numpy as np
+ from sklearn.decomposition import NMF
+ from sklearn.decomposition import PCA
+ 
+ S = mglearn.datasets.make_signals()
+ plt.figure(figsize=(6, 1))
+ plt.plot(S, '-')
+ plt.xlabel("Time")
+ plt.ylabel("Signal")
+ plt.show()
+ 
+ # mix data into a 100-dimensional state
+ A = np.random.RandomState(0).uniform(size=(100, 3))
+ X = np.dot(S, A.T)
+ print("Shape of measurements: {}".format(X.shape))
+ 
+ nmf = NMF(n_components=3, random_state=42)
+ S_ = nmf.fit_transform(X)
+ print("Recovered signal shape: {}".format(S_.shape))
+ 
+ pca = PCA(n_components=3)
+ H = pca.fit_transform(X)
+
+ models = [X, S, S_, H]
+ names = ['Observations (first three measurements)',
+  'True sources',
+  'NMF recovered signals',
+  'PCA recovered signals']
+ fig, axes = plt.subplots(4, figsize=(8, 4), gridspec_kw={'hspace': .5},
+  subplot_kw={'xticks': (), 'yticks': ()})
+ for model, name, ax in zip(models, names, axes):
+  ax.set_title(name)
+  ax.plot(model[:, :3], '-')
+
+  plt.show()
+  
+
+def FacesLargeCoefficient():
+ import numpy as np
+ from sklearn.decomposition import NMF
+ from sklearn.datasets import fetch_lfw_people
+ from sklearn.model_selection import train_test_split
+ 
+ people = fetch_lfw_people(min_faces_per_person=20, resize=0.7)
+
+ image_shape = people.images[0].shape
+
+ mask = np.zeros(people.target.shape, dtype=np.bool)
+ for target in np.unique(people.target):
+  mask[np.where(people.target == target)[0][:50]] = 1
+
+ x_people = people.data[mask]
+ y_people = people.target[mask]   
+ #Split the data into training and test sets 
+ X_train, X_test, y_train, y_test = train_test_split(x_people, y_people, stratify=y_people, random_state=0)
+ 
+ nmf = NMF(n_components=15, random_state=0)
+ nmf.fit(X_train)
+ X_train_nmf = nmf.transform(X_train)
+ X_test_nmf = nmf.transform(X_test)
+ fix, axes = plt.subplots(3, 5, figsize=(15, 12),
+  subplot_kw={'xticks': (), 'yticks': ()})
+ for i, (component, ax) in enumerate(zip(nmf.components_, axes.ravel())):
+  ax.imshow(component.reshape(image_shape))
+  
+ compn = 3
+ # sort by 3rd component, plot first 10 images
+ inds = np.argsort(X_train_nmf[:, compn])[::-1]
+ fig, axes = plt.subplots(2, 5, figsize=(15, 8),
+  subplot_kw={'xticks': (), 'yticks': ()})
+ for i, (ind, ax) in enumerate(zip(inds, axes.ravel())):
+  ax.imshow(X_train[ind].reshape(image_shape))
+ compn = 7
+ # sort by 7th component, plot first 10 images
+ inds = np.argsort(X_train_nmf[:, compn])[::-1]
+ fig, axes = plt.subplots(2, 5, figsize=(15, 8),
+  subplot_kw={'xticks': (), 'yticks': ()})
+ for i, (ind, ax) in enumerate(zip(inds, axes.ravel())):
+  ax.imshow(X_train[ind].reshape(image_shape))
+  
+  plt.show()
+   
 
 def NearestNeighborFaces():
    from sklearn.neighbors import KNeighborsClassifier
@@ -35,12 +118,12 @@ def NearestNeighborFaces():
    
    fix, axes = plt.subplots(3, 5, figsize=(15, 12), subplot_kw={'xticks': (), 'yticks': ()})
    
-   for i, (component, ax) in enumerate(zip(pca.components_, axes.ravel())):
-      ax.imshow(component.reshape(image_shape), cmap='viridis')
-      ax.set_title('{}. component'.format((i + 1)))
+   #for i, (component, ax) in enumerate(zip(pca.components_, axes.ravel())):
+      #ax.imshow(component.reshape(image_shape), cmap='viridis')
+      #ax.set_title('{}. component'.format((i + 1)))
       
-   plt.show()
-   mglearn.plots.plot_pca_faces(X_train, X_test, image_shape)
+  #plt.show()
+   mglearn.plots.plot_nmf_faces(X_train, X_test, image_shape)
    plt.show()
    
 
@@ -373,4 +456,4 @@ def main():
     
    
 if __name__ == '__main__':
-   NearestNeighborFaces()
+ ThreeSources()
