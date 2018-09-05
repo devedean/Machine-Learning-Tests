253b3fe8caaaa4c6c4ab0043fde932c001285423
devedean <devedean@yahoo.com>
2018-09-05 12:26:20 -0700

    Add files via upload

--- Intro to Machine Learning.py
+++ Intro to Machine Learning.py
@@ -1,7 +1,51 @@
 import mglearn
 import matplotlib.pyplot as plt
+import numpy as np
 
-def AgglomerativeClustering():
+
+def Adjusted_Random_Index():
+    
+ from sklearn.metrics.cluster import adjusted_rand_score
+ from sklearn.datasets import make_moons
+ from sklearn.preprocessing import StandardScaler
+ from sklearn.cluster import DBSCAN
+ from sklearn.cluster import KMeans
+ from sklearn.cluster import AgglomerativeClustering
+ 
+ 
+ 
+ X, y = make_moons(n_samples=200, noise=0.05, random_state=0)
+ 
+ # rescale the data to zero mean and unit variance
+ scaler = StandardScaler()
+ scaler.fit(X)
+ X_scaled = scaler.transform(X)
+ fig, axes = plt.subplots(1, 4, figsize=(15, 3), subplot_kw={'xticks': (), 'yticks': ()})
+ 
+ # make a list of algorithms to use
+ algorithms = [KMeans(n_clusters=2), AgglomerativeClustering(n_clusters=2),DBSCAN()]
+ 
+ # create a random cluster assignment for reference
+ random_state = np.random.RandomState(seed=0)
+ random_clusters = random_state.randint(low=0, high=2, size=len(X))
+ 
+ # plot random assignment
+ axes[0].scatter(X_scaled[:, 0], X_scaled[:, 1], c=random_clusters,cmap=mglearn.cm3, s=60)
+ axes[0].set_title("Random assignment - ARI: {:.2f}".format(adjusted_rand_score(y, random_clusters)))
+ 
+ for ax, algorithm in zip(axes[1:], algorithms):
+  
+  # plot the cluster assignments and cluster centers
+  clusters = algorithm.fit_predict(X_scaled)
+  ax.scatter(X_scaled[:, 0], X_scaled[:, 1], c=clusters,cmap=mglearn.cm3, s=60)
+  ax.set_title("{} - ARI: {:.2f}".format(algorithm.__class__.__name__,
+  adjusted_rand_score(y, clusters)))
+  
+  plt.show()
+    
+
+
+def Agglomerative_Clustering():
     from sklearn.datasets import make_blobs
     from sklearn.cluster import AgglomerativeClustering
     # Import the dendrogram function and the ward clustering function from SciPy
@@ -534,7 +578,7 @@ def Trees():
     print('Features importances:\n{}'.format(tree.feature_importances_))
 
 
-def main():
+def Linear_SVC():
     from sklearn.datasets import make_blobs
     from sklearn.svm import LinearSVC
 
@@ -552,4 +596,4 @@ def main():
 
 
 if __name__ == '__main__':
-    DB_Scan()
+    Adjusted_Random_Index()
