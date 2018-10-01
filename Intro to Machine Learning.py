8a6f09e619c31ae56e67575d7ac379527a316b5d
devedean <devedean@yahoo.com>
2018-09-30 18:20:32 -0700

    Add files via upload

--- Intro to Machine Learning.py
+++ Intro to Machine Learning.py
@@ -3,16 +3,52 @@ import matplotlib.pyplot as plt
 
 #Could be perfect for a time series prediction --  learn from the past and predict for the future
 def ExpertKnowledge():
- 
+ import numpy as np
+ from sklearn.ensemble import RandomForestRegressor
  import pandas as pd
  citibike = mglearn.datasets.load_citibike()
  print("Citi Bike data:\n{}".format(citibike.head()))
  
  # extract the target values (number of rentals)
  y = citibike.values
+ 
+ print(citibike.index.strftime("%s"))
+ 
  # convert the time to POSIX time using "%s"
- X = citibike.index.strftime("%s").astype("int").reshape(-1, 1)
+ X_hour = citibike.index.astype("int64").values.reshape(-1, 1)
+ 
+ X_hour_week = np.hstack([citibike.index.dayofweek.reshape(-1, 1),citibike.index.hour.reshape(-1, 1)])
+ 
+ regressor = RandomForestRegressor(n_estimators=100, random_state=0)
+ eval_on_features(X_hour, y, regressor) 
 
+
+def eval_on_features(features, target, regressor):
+  
+  # use the first 184 data points for training, and the rest for testing
+  n_train = 184
+  # function to evaluate and plot a regressor on a given feature set  
+  
+  # split the given features into a training and a test set
+  X_train, X_test = features[:n_train], features[n_train:]
+  # also split the target array
+  y_train, y_test = target[:n_train], target[n_train:]
+  regressor.fit(X_train, y_train)
+  print("Test-set R^2: {:.2f}".format(regressor.score(X_test, y_test)))
+  y_pred = regressor.predict(X_test)
+  y_pred_train = regressor.predict(X_train)
+  plt.figure(figsize=(10, 3))
+  #plt.xticks(range(0, len(features), 8), xticks.strftime("%a %m-%d"), rotation=90,ha="left")
+  plt.plot(range(n_train), y_train, label="train")
+  plt.plot(range(n_train, len(y_test) + n_train), y_test, '-', label="test")
+  plt.plot(range(n_train), y_pred_train, '--', label="prediction train")
+  plt.plot(range(n_train, len(y_test) + n_train), y_pred, '--',
+  label="prediction test")
+  plt.legend(loc=(1.01, 0))
+  plt.xlabel("Date")
+  plt.ylabel("Rentals")
+  plt.show()
+  
 
 def UnivariateStatistics():
  from sklearn.datasets import load_breast_cancer
