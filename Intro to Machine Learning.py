40170588980c66b135a9e635fdff8c543e40a8fc
devedean <devedean@yahoo.com>
2018-10-04 19:50:17 -0700

    Add files via upload

--- Intro to Machine Learning.py
+++ Intro to Machine Learning.py
@@ -3,13 +3,9 @@ import matplotlib.pyplot as plt
 
 
 def CrossValidation():
- from sklearn.model_selection import cross_val_score
  from sklearn.datasets import load_iris
- from sklearn.linear_model import LogisticRegression
  iris = load_iris()
- logreg = LogisticRegression()
- scores = cross_val_score(logreg, iris.data, iris.target, cv=5)
- print("Average cross-validation score: {:.2f}".format(scores.mean()))
+ print("Iris labels:\n{}".format(iris.target))
 
 
 #Could be perfect for a time series prediction --  learn from the past and predict for the future
