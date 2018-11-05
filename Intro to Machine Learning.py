6ce4373be2279051ab8160c3a4194fe7469c7313
devedean <devedean@yahoo.com>
2018-11-04 16:53:16 -0800

    Add files via upload

--- Intro to Machine Learning.py
+++ Intro to Machine Learning.py
@@ -1,6 +1,41 @@
 import mglearn
 import matplotlib.pyplot as plt
 
+
+def ImbalancedDatasets():
+ from sklearn.datasets import load_digits
+ from sklearn.model_selection import train_test_split 
+ from sklearn.dummy import DummyClassifier
+ import numpy as np
+ from sklearn.linear_model import LogisticRegression
+ from sklearn.metrics import confusion_matrix
+  
+ digits = load_digits()
+ y = digits.target == 9
+ X_train, X_test, y_train, y_test = train_test_split(digits.data, y, random_state=0) 
+
+
+ dummy_majority = DummyClassifier(strategy='most_frequent').fit(X_train, y_train)
+ pred_most_frequent = dummy_majority.predict(X_test)
+ print("Unique predicted labels: {}".format(np.unique(pred_most_frequent)))
+ print("Test score: {:.2f}".format(dummy_majority.score(X_test, y_test)))
+
+ dummy = DummyClassifier().fit(X_train, y_train)
+ pred_dummy = dummy.predict(X_test)
+ print("dummy score: {:.2f}".format(dummy.score(X_test, y_test)))
+ logreg = LogisticRegression(C=0.1).fit(X_train, y_train)
+ pred_logreg = logreg.predict(X_test)
+ print("logreg score: {:.2f}".format(logreg.score(X_test, y_test)))
+
+ confusion = confusion_matrix(y_test, pred_logreg)
+ print("Confusion matrix:\n{}".format(confusion))
+ 
+ from sklearn.metrics import classification_report
+ print(classification_report(y_test, pred_most_frequent,target_names=["not nine", "nine"]))
+ print(classification_report(y_test, pred_dummy,target_names=["not nine", "nine"]))
+ print(classification_report(y_test, pred_logreg,target_names=["not nine", "nine"]))
+ 
+
 def ParameterGrid():
  import numpy as np
  from sklearn.datasets import load_iris
@@ -859,4 +894,4 @@ def main():
 
 
 if __name__ == '__main__':
- ParameterGrid()
+ ImbalancedDatasets()
