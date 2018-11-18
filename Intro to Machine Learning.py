ec39af15e90f84a87fca5f9adc99d5746c86bebe
devedean <devedean@yahoo.com>
2018-11-17 19:28:11 -0800

    Add files via upload

--- Intro to Machine Learning.py
+++ Intro to Machine Learning.py
@@ -2,6 +2,39 @@ import mglearn
 import matplotlib.pyplot as plt
 
 
+def AlgorithmChainsandPipelines():
+ from sklearn.svm import SVC
+ from sklearn.pipeline import Pipeline
+ from sklearn.datasets import load_breast_cancer
+ from sklearn.model_selection import train_test_split
+ from sklearn.preprocessing import MinMaxScaler
+ from sklearn.model_selection import GridSearchCV
+ # load and split the data
+ cancer = load_breast_cancer()
+ X_train, X_test, y_train, y_test = train_test_split(cancer.data, cancer.target, random_state=0)
+ # compute minimum and maximum on the training data
+ scaler = MinMaxScaler().fit(X_train)
+ # rescale the training data
+ X_train_scaled = scaler.transform(X_train)
+ svm = SVC()
+ # learn an SVM on the scaled training data
+ svm.fit(X_train_scaled, y_train)
+ # scale the test data and score the scaled data
+ X_test_scaled = scaler.transform(X_test)
+ print("Test score: {:.2f}".format(svm.score(X_test_scaled, y_test))) 
+ pipe = Pipeline([("scaler", MinMaxScaler()), ("svm", SVC())])
+ pipe.fit(X_train, y_train)
+ print("Test score: {:.2f}".format(pipe.score(X_test, y_test)))
+ 
+ param_grid = {'svm__C': [0.001, 0.01, 0.1, 1, 10, 100],'svm__gamma': [0.001, 0.01, 0.1, 1, 10, 100]}
+ grid = GridSearchCV(pipe, param_grid=param_grid, cv=5)
+ grid.fit(X_train, y_train)
+ print("Best cross-validation accuracy: {:.2f}".format(grid.best_score_))
+ print("Test set score: {:.2f}".format(grid.score(X_test, y_test)))
+ print("Best parameters: {}".format(grid.best_params_)) 
+
+
+
 def ImbalancedDatasets():
  from sklearn.datasets import load_digits
  from sklearn.model_selection import train_test_split 
@@ -894,4 +927,4 @@ def main():
 
 
 if __name__ == '__main__':
- ImbalancedDatasets()
+ AlgorithmChainsandPipelines()
